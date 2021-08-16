3561f897a664a147dcb5e755df86137a6ddfb500
TsurikViktor <tsurik.viktor.vladimirovich@gmail.com>
2021-08-16 17:14:21 +0300

    commit ispravlenie

--- js/customs.js
+++ js/customs.js
@@ -1,34 +1,35 @@
 var changeFunction;
-var red;
-var green;
-var blue;
-var product;
 
-function rgb(red, green, blue) {
+function rgb(red = 0, green = 0, blue = 0) {
+    red = +prompt(`Введите значение red -->`); 
+    green = +prompt(`Введите значение green  -->`); 
+    blue = +prompt(`Введите значение blue -->`); 
     if (!red) red = 0;
     if (!green) green = 0;
     if (!blue) blue = 0;
-    return alert(`RGB( ${red}, ${green}, ${blue})`);
+    console.log(`RGB( ${red}, ${green}, ${blue})`);
+    return `RGB( ${red}, ${green}, ${blue})`;
+
 };
 
-function words(product) {
+function words(product = 0) {
+    product = +prompt(`Введите кол-во товаров --->`);
     if (!product) product = 0;
     var value = Math.abs(product) % 100;
     var num = value % 10;
     if (value > 10 && value < 20) return alert(`${product} товаров`);
     if (num > 1 && num < 5) return alert(`${product} товара`);
     if (num == 1) return alert(`${product} товар`);
-    return alert(`${product} товаров`);
+    console.log(`${product} товаров`);
+    return `${product} товаров`;
+    
 };
 
 changeFunction = confirm('для вызова функции rgb() нажмите (OK), для вызова функции words() нажмите (Отмена)');
 
 if (!changeFunction) {
-    product = +prompt(`Введите кол-во товаров --->`);
-    words(product);
+    words();
 } else {
-    red = +prompt(`Введите значение red -->`); 
-    green = +prompt(`Введите значение green 2 -->`); 
-    blue = +prompt(`Введите значение blue -->`); 
-    rgb (red, green, blue);
+    rgb();
 };
+
