0ca13d2a57c031ada657155a764bf226eff30246
TsurikViktor <tsurik.viktor.vladimirovich@gmail.com>
2021-08-16 20:15:04 +0300

    commit ispravlenie 4

--- js/customs.js
+++ js/customs.js
@@ -11,18 +11,18 @@ function words(product) {
     if (!product) product = 0;
     var value = Math.abs(product) % 100;
     var num = value % 10;
-    if (value > 10 && value < 20) return alert(`${product} товаров`);
-    if (num > 1 && num < 5) return alert(`${product} товара`);
-    if (num == 1) return alert(`${product} товар`);
-    return `${product} товаров`;
+    if (value > 10 && value < 20) return product + ' товаров';
+    if (num > 1 && num < 5) return product + ' товара';
+    if (num == 1) return product + ' товар';
+    return product + ' товаров';
 };
 
 changeFunction = confirm('для вызова функции rgb() нажмите (OK), для вызова функции words() нажмите (Отмена)');
 
 if (!changeFunction) {
-    var productWords = words(+prompt(`Введите кол-во товаров --->`));
+    var productWords = words( +prompt(`Введите кол-во товаров --->`));
     console.log(productWords);
 } else {
-    var rgbColor = rgb(+prompt(`Введите значение red -->`), +prompt(`Введите значение green  -->`), +prompt(`Введите значение blue -->`));
+    var rgbColor = rgb( +prompt(`Введите значение red -->`), +prompt(`Введите значение green  -->`), +prompt(`Введите значение blue -->`));
     console.log(rgbColor);
 };
